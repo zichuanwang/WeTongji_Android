348294f6e6744e97cb184d936ec83fc06a8c082b
Tony <coolhzb@163.com>
2013-04-22 22:33:52 +0800

    notification part add list adapter and layout files

--- WeTongji/src/com/wetongji_android/ui/notification/NotificationFragment.java
+++ WeTongji/src/com/wetongji_android/ui/notification/NotificationFragment.java
@@ -6,7 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class NotificationFragment extends Fragment
+public class NotificationFragment extends Fragment 
 {
 
 	@Override
@@ -17,18 +17,18 @@ public class NotificationFragment extends Fragment
 	}
 
 	@Override
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) 
 	{
 		// TODO Auto-generated method stub
 		return super.onCreateView(inflater, container, savedInstanceState);
-	}
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) 
-	{
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
 	}
 
 	@Override
