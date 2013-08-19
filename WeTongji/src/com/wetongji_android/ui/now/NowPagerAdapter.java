22ff62664f7eb97c41fdf3d1de83d1e07c198b38
Guangyu <Guangyu.Gao@partner.bmw.com>
2013-08-19 15:01:08 +0800

    Now页面增加缓存

--- WeTongji/src/com/wetongji_android/ui/now/NowPagerAdapter.java
+++ WeTongji/src/com/wetongji_android/ui/now/NowPagerAdapter.java
@@ -3,6 +3,7 @@ package com.wetongji_android.ui.now;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 import android.content.Context;
 import android.content.Intent;
@@ -37,41 +38,44 @@ import com.wetongji_android.util.net.ApiHelper;
 import com.wetongji_android.util.net.HttpRequestResult;
 
 public class NowPagerAdapter extends PagerAdapter {
-	
-	public static final int PAGE_LEFT=0;
-	public static final int PAGE_RIGHT=2;
-	public static final int PAGE_MIDDILE=1;
-	
-	private LayoutInflater inflater;
-	private Fragment fragment;
-	private ViewPager viewPager;
+
+	public static final int PAGE_LEFT = 0;
+	public static final int PAGE_RIGHT = 2;
+	public static final int PAGE_MIDDILE = 1;
+
+	private final LayoutInflater inflater;
+	private final Fragment fragment;
+	private final ViewPager viewPager;
 	private NowWeekListAdapter listAdapter;
 	private AmazingListView nowWeekList;
 	private PageNetworkLoaderCallbacks callbacks;
 	private Calendar begin, end, min, max;
-	private Context context;
-	
+	private final Context context;
+
+	private static ConcurrentHashMap<String, List<Event>> historyEventsData = new ConcurrentHashMap<String, List<Event>>();
+
 	public NowPagerAdapter(Fragment fragment, ViewPager viewPager) {
 		super();
-		this.fragment=fragment;
-		context=fragment.getActivity();
-		this.inflater=fragment.getActivity().getLayoutInflater();
-		this.viewPager=viewPager;
+		this.fragment = fragment;
+		context = fragment.getActivity();
+		this.inflater = fragment.getActivity().getLayoutInflater();
+		this.viewPager = viewPager;
 		initCalendars();
 		initAdapters();
 		initCallbacks();
-		Bundle args=ApiHelper.getInstance(context).getSchedule(begin, end);
+		Bundle args = ApiHelper.getInstance(context).getSchedule(begin, end);
 		Log.v("nowpager", "initloader");
+
 		fragment.getLoaderManager().initLoader(WTApplication.NETWORK_LOADER_1, args, callbacks).forceLoad();
-		
+
 	}
 
 	private void initAdapters() {
-		listAdapter=new NowWeekListAdapter(fragment, begin, end);
+		listAdapter = new NowWeekListAdapter(fragment, begin, end);
 	}
-	
-	private void initCallbacks(){
-		callbacks=new PageNetworkLoaderCallbacks();
+
+	private void initCallbacks() {
+		callbacks = new PageNetworkLoaderCallbacks();
 	}
 
 	@Override
@@ -81,16 +85,17 @@ public class NowPagerAdapter extends PagerAdapter {
 
 	@Override
 	public Object instantiateItem(ViewGroup container, int position) {
-		switch(position){
+		switch (position) {
 		case PAGE_LEFT:
 		case PAGE_RIGHT:
-			View view=inflater.inflate(R.layout.page_now_loading, null);
+			View view = inflater.inflate(R.layout.page_now_loading, null);
 			container.addView(view);
 			return view;
 		default:
-			nowWeekList=(AmazingListView) inflater.inflate(R.layout.page_now_week, null);
+			nowWeekList = (AmazingListView) inflater.inflate(R.layout.page_now_week, null);
 			nowWeekList.setAdapter(listAdapter);
-			View pinnedHeader=LayoutInflater.from(context).inflate(R.layout.section_bar_now, nowWeekList, false);
+			View pinnedHeader = LayoutInflater.from(context).inflate(R.layout.section_bar_now, nowWeekList,
+					false);
 			nowWeekList.setPinnedHeaderView(pinnedHeader);
 			nowWeekList.setOnItemClickListener(new ListItemClickListener());
 			container.addView(nowWeekList);
@@ -105,80 +110,91 @@ public class NowPagerAdapter extends PagerAdapter {
 
 	@Override
 	public boolean isViewFromObject(View arg0, Object arg1) {
-		return arg0==arg1;
+		return arg0 == arg1;
 	}
 
 	@Override
 	public int getItemPosition(Object object) {
 		return POSITION_NONE;
 	}
-	
-	private void initCalendars(){
-		min=begin=DateParser.getFirstDayOfWeek();
-		max=end=DateParser.getLastDayOfWeek();
+
+	private void initCalendars() {
+		min = begin = DateParser.getFirstDayOfWeek();
+		max = end = DateParser.getLastDayOfWeek();
 	}
-	
-	private void setCalendars(int pageScrolledTo){
-		switch(pageScrolledTo){
+
+	private void setCalendars(int pageScrolledTo) {
+		switch (pageScrolledTo) {
 		case PAGE_LEFT:
 			Log.v("setcalendar", "page left");
 			begin.add(Calendar.DAY_OF_YEAR, -7);
 			end.add(Calendar.DAY_OF_YEAR, -7);
-			if(min.after(begin))
-				min=begin;
+			if (min.after(begin))
+				min = begin;
 			break;
 		case PAGE_RIGHT:
 			Log.v("setcalendar", "page right");
 			begin.add(Calendar.DAY_OF_YEAR, 7);
 			end.add(Calendar.DAY_OF_YEAR, 7);
-			if(end.after(max))
-				max=end;
+			if (end.after(max))
+				max = end;
 			break;
 		}
 	}
-	
-	public void setContent(int pageScrolledTo){
-		if(pageScrolledTo!=PAGE_MIDDILE){
+
+	public void setContent(int pageScrolledTo) {
+		if (pageScrolledTo != PAGE_MIDDILE) {
 			setCalendars(pageScrolledTo);
-			if(begin.equals(min)||end.equals(max)){
-				Bundle args=ApiHelper.getInstance(context).getSchedule(begin, end);
-				fragment.getLoaderManager().restartLoader(WTApplication.NETWORK_LOADER_1, args, callbacks).forceLoad();
-			}
-			else{
+			if (begin.equals(min) || end.equals(max)) {
+
+				if (!historyEventsData.containsKey(begin.get(Calendar.YEAR) + ""
+						+ begin.get(Calendar.DAY_OF_YEAR))) {
+					Bundle args = ApiHelper.getInstance(context).getSchedule(begin, end);
+					fragment.getLoaderManager()
+							.restartLoader(WTApplication.NETWORK_LOADER_1, args, callbacks).forceLoad();
+				} else {
+					listAdapter.setRawData(historyEventsData.get(begin.get(Calendar.YEAR) + ""
+							+ begin.get(Calendar.DAY_OF_YEAR)));
+					System.out.println("Calendar begin:" + begin.get(Calendar.YEAR) + ""
+							+ begin.get(Calendar.DAY_OF_YEAR));
+					viewPager.setCurrentItem(PAGE_MIDDILE, false);
+				}
+			} else {
 				initAdapters();
 				nowWeekList.setAdapter(listAdapter);
 			}
 		}
 	}
-	
-	private class PageNetworkLoaderCallbacks implements LoaderCallbacks<HttpRequestResult>{
-		
-		private EventFactory factory;
-		
+
+	private class PageNetworkLoaderCallbacks implements LoaderCallbacks<HttpRequestResult> {
+
+		private final EventFactory factory;
+
 		public PageNetworkLoaderCallbacks() {
 			super();
-			this.factory=new EventFactory(fragment);
+			this.factory = new EventFactory(fragment);
 		}
-		
+
 		@Override
 		public Loader<HttpRequestResult> onCreateLoader(int arg0, Bundle args) {
 			return new NetworkLoader(context, HttpMethod.Get, args);
 		}
 
 		@Override
-		public void onLoadFinished(Loader<HttpRequestResult> arg0,
-				HttpRequestResult result) {
-			if(result.getResponseCode()==0){
-				List<Event> events=new ArrayList<Event>();
-				if(begin.equals(DateParser.getFirstDayOfWeek())){
-					events=factory.createObjects(result.getStrResponseCon(), true);
-				}
-				else{
-					events=factory.createObjects(result.getStrResponseCon(), false);
+		public void onLoadFinished(Loader<HttpRequestResult> arg0, HttpRequestResult result) {
+			if (result.getResponseCode() == 0) {
+				List<Event> events;
+				if (begin.equals(DateParser.getFirstDayOfWeek())) {
+					events = new ArrayList<Event>(factory.createObjects(result.getStrResponseCon(), true));
+				} else {
+					events = new ArrayList<Event>(factory.createObjects(result.getStrResponseCon(), false));
 				}
 				listAdapter.setRawData(events);
-			}
-			else{
+				System.out.println(begin.get(Calendar.YEAR) + "" + begin.get(Calendar.DAY_OF_YEAR)
+						+ " Calendar is saved");
+				historyEventsData
+						.put(begin.get(Calendar.YEAR) + "" + begin.get(Calendar.DAY_OF_YEAR), events);
+			} else {
 				ExceptionToast.show(context, result.getResponseCode());
 			}
 			viewPager.setCurrentItem(PAGE_MIDDILE, false);
@@ -187,18 +203,17 @@ public class NowPagerAdapter extends PagerAdapter {
 		@Override
 		public void onLoaderReset(Loader<HttpRequestResult> arg0) {
 		}
-		
+
 	}
-	
+
 	private class ListItemClickListener implements OnItemClickListener {
 
 		@Override
-		public void onItemClick(AdapterView<?> arg0, View view, int position,
-				long id) {
+		public void onItemClick(AdapterView<?> arg0, View view, int position, long id) {
 			Event event = listAdapter.getItem(position);
 			Bundle bundle = new Bundle();
 			Intent intent = null;
-			if(event instanceof Activity){
+			if (event instanceof Activity) {
 				intent = new Intent(context, EventDetailActivity.class);
 				bundle.putParcelable(EventsFragment.BUNDLE_KEY_ACTIVITY, event);
 				intent.putExtras(bundle);
@@ -209,7 +224,7 @@ public class NowPagerAdapter extends PagerAdapter {
 			}
 			fragment.startActivity(intent);
 		}
-		
+
 	}
-	
+
 }
